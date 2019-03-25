7593932e6442f6b60db6cd2b488af4dff6f01efc
sijan <sijanshrestha2@gmail.com>
2019-03-25 15:19:47 +0700

    WIP

---
+++ src/Api.ts
@@ -0,0 +1,47 @@
+import {Client, IFetchRequest, Service} from "@crazyfactory/tinka";
+import {ContentTypeMiddleware} from "./middlewares/ContentTypeMiddleware";
+import {WrapMiddleware} from "./middlewares/WrapMiddleware";
+import {DepositReport} from "./nodes/DepositReport";
+import {PeakMonthly} from "./nodes/PeakMonthly";
+
+export class Api extends Service {
+  private static instances: { [key: string]: Api } = {};
+
+  public static getInstance(endpoint: string = Api.getEndpoint()): Api {
+    if (!Api.instances[endpoint]) {
+      Api.instances[endpoint] = new Api(Api.setupMiddlewares(Api.getClient(endpoint)));
+    }
+    return Api.instances[endpoint];
+  }
+
+  public static getClient(baseUrl: string): Client {
+    const request: IFetchRequest = {baseUrl};
+    return new Client(request);
+  }
+
+  public static getEndpoint(): string {
+    // tslint:disable-next-line
+    console.log("process.env", process.env);
+    if (process.env.NODE_ENV === "development") {
+      return "http://localhost:30000"; //tslint:disable-line
+    }
+    if (process.env.NODE_ENV === "production") {
+      return "https://services.bchurunway.com";//tslint:disable-line
+    }
+    alert("NODE_ENV NOT SET");
+  }
+
+  public get depositReturn(): DepositReport {
+    return new DepositReport(this.client);
+  }
+
+  public get peakMonthly(): PeakMonthly {
+    return new PeakMonthly(this.client);
+  }
+
+  private static setupMiddlewares(client: Client): Client {
+    client.addMiddleware(new WrapMiddleware());
+    client.addMiddleware(new ContentTypeMiddleware());
+    return client;
+  }
+}
